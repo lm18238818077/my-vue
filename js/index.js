13ab0089e5781da3b23fdc7940ec4110313fc186
liming <18003554931@163.com>
2021-08-04 22:44:38 +0800

    feat✨:  解析

--- js/index.js
+++ js/index.js
@@ -24,3 +24,8 @@ Mvue.prototype = {
     });
   },
 };
+
+// 原理
+//vue采用数据劫持，配合发布订阅模式，通过object.definerproper来劫持各个属性的setter，getter，在数据变动时通知消息订阅器，去通知观察者，触发对应的回调函数，来更新视图
+
+//mvvm作为绑定的入口，整合observer，complier，和watcher三者，通过observer来监听modal数据变化，通过complier来解析编译指令，最总利用watcher搭起observer，comlier之间的通信桥梁，达到数据变化-》视图更新，视图交互变化-》数据modal变更的双向绑定效果。
